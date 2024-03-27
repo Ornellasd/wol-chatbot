6d765fab62409dfc150b29851f09975e218e9fb7
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2024-03-27 14:48:01 +0100

    adds ESP8266WebServer library to test out wol

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,35 @@
 #include <ESP8266WiFi.h>
-
+#include <ESP8266WebServer.h>
+#include <WakeOnLan.h>
 #include "settings.h"
+
+WiFiUDP UDP;
+WakeOnLan WOL(UDP);
+ESP8266WebServer server(80);
 
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
 
+const char* MACAddress = MAC_ADDRESS;
+
 void beginWifi();
+void beginServer();
+void handleRoot();
+
+void beginServer() {
+    server.on("/", handleRoot);
+    server.begin();
+    Serial.println("HTTP server started.");
+}
 
 void setup() {
     Serial.begin(9600);
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
     beginWifi();
+    beginServer();
+    // 
+    Serial.println(MACAddress);
 }
 
 void beginWifi() {
@@ -32,6 +50,15 @@ void beginWifi() {
     Serial.println(WiFi.localIP());
 }
 
-void loop() {
+void wakeServer() {
+    WOL.sendMagicPacket(MACAddress);
+}
 
+void handleRoot() {
+    server.send(200, "text/plain", "HALLOTJES!, waking server..");
+    wakeServer();
+}
+
+void loop() {
+    server.handleClient();
 }
